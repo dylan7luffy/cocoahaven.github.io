72625c30d12210a468f04d4570c6c1a1ddb84a47
CarterRigert <89533142+CarterRigert@users.noreply.github.com>
2024-05-20 22:11:43 -0600

    updated background

--- nova/src/components/Header.js
+++ nova/src/components/Header.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './Css/Header.css';
-import PaytonImage from '../images/background.jpg';
+import background from '../images/background.png';
 
 
 const Header = () => {
     return (
         <header
             className="header text-white text-center py-5"
-            style={{ backgroundImage: `url(${PaytonImage})` }}
+            style={{ backgroundImage: `url(${background})` }}
         >
             <div className="container">
                 <div className="header-content">
-                    <h1>Nova Public Health Consulting</h1>
+                    <h1>Cocoa Haven</h1>
                     <h2>Empowering Healthier Futures, Together</h2>
                     <p>
-                        Owned and Operated by Payton Rigert
+                        Owned and Operated by Dylan Polewczyk
                     </p>
 
                 </div>
